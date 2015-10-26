3f1e965bebb41b7d07cf4450186a3506e8a6ce4d
TimTheSinner <timthesinner@gmail.com>
2015-10-25 23:37:32 -0600

    Naieve implementation of a promise throttle with random backoff

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # throttle-promised
+Throttle promise execution to a maximum count:
+- Execution order is not guaranteed
+- Uses random backoff at a configurable rate to re-queue promises
+
+## Getting Started
+
+```shell
+npm install throttle-promised --save
+```
+
+## Example throttling 100 promoses through 10 executors
+```js
+var _ = require('underscore'),
+    throttle = require('throttle-promised')({max:10, minWait:10, maxWait:100});
+    
+_.each(range(100), function(i) {
+  throttle(function() {
+    return Q.promise(function(resolve, reject) {
+      setTimeout(function() {
+        resolve(i);
+      }, 100);
+    });
+  });
+});
+```
---
+++ index.js
@@ -0,0 +1,56 @@
+//Copyright (c) 2015 TimTheSinner All Rights Reserved.
+'use strict';
+
+/**
+ * Copyright (c) 2015 TimTheSinner All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * @author TimTheSinner
+ */
+var Q = require('q');
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function Throttle(max, minWait, maxWait) {
+  this.active = 0;
+  this.max = max;
+  this.minWait = minWait || 50;
+  this.maxWait = maxWait || 500;
+}
+module.exports = function(properties) {
+  if (! properties) {
+    return new Throttle();
+  } else {
+    return new Throttle(properties.max, properties.minWait, properties.maxWait);
+  }
+}
+    
+Throttle.prototype.release = function(cb, res) {
+  this.active -= 1;
+  cb(res);
+}
+
+Throttle.prototype.throttle = function(fn) {
+  if (this.active === this.max) {
+    return Q.delay(random(this.minWait, this.maxWait)).then(this.throttle.bind(this, fn));
+  } else {
+    this.active += 1;
+    return Q.promise(function (resolve, reject, notify) {
+      fn().then(this.release.bind(this, resolve))
+          .fail(this.release.bind(this, reject));
+    });
+  }
+}
